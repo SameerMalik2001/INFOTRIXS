3a59adebe3f6fc1cb9e5a6d57be24afba40ffe96
Sameer Malik <99330095+SameerMalik2001@users.noreply.github.com>
2023-07-08 09:35:24 +0530

    Update contact_management_system.py

--- Week1-tasks/contact_management_system.py
+++ Week1-tasks/contact_management_system.py
@@ -1,13 +1,18 @@
 #to print contact list
 def printlist(contact_list):
     print("Contacts:-")
+    print("     Name","                                               ","Number")
+    print()
     if len(contact_list) > 0:
         i = 1
         for key, value in sorted(contact_list.items()):
             Number = str(value)
             if len(Number) == 10:
                 Number = Number[:3] + "-" + Number[3:6] + "-" + Number[6:]
-            print(i,"->",str(key).capitalize(),"   ", Number)
+                space = ""
+                for j in range(len(key),51):
+                    space = space + " "
+            print(i,"->",key,space, Number)
             i = i + 1
     else:
         print("No Contact")
@@ -15,18 +20,39 @@ def printlist(contact_list):
 #to take number with validation
 def take_number_by_validation():
     loop = True
-    while(loop):
+    while loop:
         try:
-            Number = int(input("Enter the new Number: "))
+            Number = int(input("Enter the Number: "))
+            if len(str(Number)) != 10:
+                print("Number should be of 10 digit.")
+                continue
             loop = False
         except Exception as e:
             print("Number can't contain letters.")
             loop = True
+        
+
     return Number
+
+#to remove leading spaces from name, if user add that by mistake
+def trim_name(Name):
+    Name1 = Name
+    for i in range(0,len(Name)):
+        if Name[i] == " ":
+            Name1 = Name1[1:]
+        else:
+            break
+    break_point = 0
+    for i in range(0,len(Name1)):
+        if Name1[i] != " ":
+            break_point = i
+    if break_point+1 != len(Name1):
+        Name1 = Name1[:break_point+1]
+    return Name1
 
 #to store contacts
 contact_list = {}
-while(1):
+while 1:
     print("press 1: Display Contacts")
     print("press 2: Add Contact")
     print("press 3: Update Contact")
@@ -35,7 +61,7 @@ while(1):
     print("press 0: To Exit")
 
     #handle input data that it should be integer. 
-    while(1):
+    while 1:
         try:
             choise = int(input("Enter your choice here: "))
             break
@@ -56,13 +82,16 @@ while(1):
 
         #add number
         elif choise == 2:
-            while(1):
+            while 1:
                 Name = str(input("Enter the Name: "))
-                if Name not in contact_list:
+                if Name in contact_list:
+                    print("Name is taken. choose another.")
+                elif len(Name) > 40:
+                    print("Name is too long. Try short Name.")
+                elif Name not in contact_list:
                     break
-                print("Name is taken. choose another.")
             Number = take_number_by_validation()
-            contact_list[Name] = Number
+            contact_list[trim_name(Name)] = Number
             print("Add Successfully.")
 
         #update contact
@@ -91,8 +120,13 @@ while(1):
         #search contact
         else:
             Name = str(input("Enter the Name: "))
+            print()
+            print("Name","                                               ","Number")
             if Name in contact_list:
-                print(Name,"   ", contact_list[Name])
+                space = ""
+                for j in range(len(Name),51):
+                    space = space + " "
+                print(Name,space, contact_list[Name])
             else:
                 print("Contact is not found.")
     print()
